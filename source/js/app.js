5fdc55fc0c782394c9a0e8370688b97161e7d55b
ilya barmin <isBarmin@yandex.ru>
2016-09-30 21:14:26 +0500

    made the main navigation

--- source/js/app.js
+++ source/js/app.js
@@ -2,6 +2,37 @@ window.onload = function () {
 
   (function() {
 
+    $(document).on('click', '.nav__trigger', function(e) {
+      var trigger = $(this);
+      var nav     = trigger.closest('.nav');
+      var drop    = nav.find('.nav__drop');
+
+      if(nav.hasClass('nav--open')) {
+
+        drop.fadeOut( 500 , function() {
+          nav.removeClass('nav--open');
+        });
+
+        $('body').css('overflow', '');
+
+      } else {
+
+        drop.show(0, function() {
+          nav.addClass('nav--open');
+        });
+        $('body').css('overflow', 'hidden');
+
+      }
+    });
+
+  })();
+
+
+
+
+
+  (function() {
+
     if( !document.getElementById('map') ) return;
 
     var map;
