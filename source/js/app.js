5a7f173ee59597606e7f8b1e23b55402912a2719
ilya barmin <isBarmin@yandex.ru>
2016-10-02 16:35:26 +0500

    updated map

--- source/js/app.js
+++ source/js/app.js
@@ -104,19 +104,154 @@ window.onload = function () {
   // Карта
   (function() {
 
-    if( !document.getElementById('map') ) return;
+    var map = document.getElementById('map');
 
-    var map;
+    if( map.length === 0 ) return;
 
-    DG.then(function () {
-        map = DG.map('map', {
-            center: [57.925, 56.05],
-            zoom: 11,
-              zoomControl: false,
-              scrollWheelZoom: false,
-              fullscreenControl: false
-        });
-    });
+
+    // параметры карты
+    var latitude  = 58.0223849;
+    var longitude = 56.2338462;
+    var mapZoom  = 15;
+
+
+    // Стилизация карты
+    var mainСolor = "#61DAC9";
+
+    var style = [
+      {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#444444"
+          }
+        ]
+      },
+      {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+          {
+            "color": "#f2f2f2"
+          }
+        ]
+      },
+      {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#ffffff"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+          {
+            "saturation": -100
+          },
+          {
+            "lightness": 45
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#d6d6d6"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+          {
+            "visibility": "simplified"
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+          {
+            "color": "#46bcec"
+          },
+          {
+            "visibility": "on"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": mainСolor
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      }
+    ]
+
+
+
+    // Настройки карты
+    var mapOptions = {
+      center: new google.maps.LatLng(latitude, longitude),
+      zoom: mapZoom,
+      panControl: false,
+      zoomControl: false,
+      mapTypeControl: false,
+      streetViewControl: false,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      scrollwheel: false,
+      styles: style,
+    }
+
+    // Инициализация карты
+    var googleMap = new google.maps.Map(map, mapOptions);
 
   })();
 
