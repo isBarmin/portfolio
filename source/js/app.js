af438d4fe6b76a1aa1de773731cb35ebe81c7834
ilya barmin <isBarmin@yandex.ru>
2016-10-07 13:31:22 +0500

    added preloader

--- source/js/app.js
+++ source/js/app.js
@@ -1,4 +1,67 @@
-window.onload = function () {
+$( document ).ready(function() {
+
+  // preloader
+  (function() {
+
+    var imgs = [];
+
+    $('*').each(function () {
+      var $this      = $(this);
+      var background = $this.css('background-image');
+      var isImg      = $this.is('img');
+
+      if (background !== 'none') {
+        var path = background.replace('url("', '').replace('")', '');
+
+        if( path.indexOf('-gradient(') !== -1 ) return;
+
+        imgs.push(path);
+      }
+
+      if (isImg) {
+        var path = $this.attr('src');
+
+        if (!path) return;
+        imgs.push(path);
+      }
+    });
+
+
+    var percentsTotal = 1;
+
+    for (var i = 0; i < imgs.length; i++) {
+      var image = $('<img>', {
+        attr: {
+          src: imgs[i]
+        }
+      });
+
+      image.on({
+        load : function () {
+          setPercents(imgs.length, percentsTotal);
+          percentsTotal++;
+        },
+        error : function () {
+          percentsTotal++;
+        }
+      });
+    }
+
+    function setPercents(total, current) {
+      var percent = Math.ceil(current / total * 100);
+
+      if (percent >= 100) {
+        $('.preloader').fadeOut();
+      }
+
+      $('.preloader__value').text(percent);
+    }
+
+  })();
+
+
+
+
 
   // Работа главного меню
   (function() {
@@ -297,4 +360,4 @@ window.onload = function () {
 
   })();
 
-};
+});
